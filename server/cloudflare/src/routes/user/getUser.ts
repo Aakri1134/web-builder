1271a837a81a51a5cce8ba3697cbcf32b3003d82
Aakri1134 <aakrisri55@gmail.com>
2025-06-27 15:48:54 +0530

    Add /getuser

--- server/cloudflare/src/routes/user/getUser.ts
+++ server/cloudflare/src/routes/user/getUser.ts
@@ -3,14 +3,16 @@ import { Env } from "../..";
 import userInputValidation from "../../middlewares/auth/userInputValidation";
 import { jwtVerification } from "../../middlewares/user/jwtVerification";
 import { getUserMiddleware } from "../../middlewares/auth/getUser";
+import { midIPRateLimiting } from "../../middlewares/auth/ipratelimiting";
 
 export const getUser = new Hono<Env>()
 
-getUser.get("/", userInputValidation, jwtVerification, getUserMiddleware,async (c) => {
+getUser.get("/", midIPRateLimiting, userInputValidation, jwtVerification, getUserMiddleware,async (c) => {
 
-    
-
+    const user = c.get("user")
+    const {password, previousPassword, emailVerified, ...res} = user
     return c.json({
-        message : "User"
+        success : true,
+        data : res
     })
 })
---
+++ server/cloudflare/src/routes/user/prompt.ts
@@ -0,0 +1,5 @@
+import { createMiddleware } from "hono/factory";
+
+export const prompt = createMiddleware(async(c, next) => {
+    // TODO Last Change Made : GetUser, to continue work on prompting and so on
+})
