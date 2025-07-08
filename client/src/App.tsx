a14d236d02b4ae0e78dde9a3dc678dbec7dc0a91
Aakri1134 <aakrisri55@gmail.com>
2025-07-08 13:11:03 +0530

    Add modal portal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
-function App() {
+import { useState } from "react"
+import ModalPortal from "./components/modals/ModalPortal"
 
-  return <></>
+function App() {
+  const [tr, settr] = useState<boolean>(false)
+
+  return <>
+  {tr && <ModalPortal hideModal={()=>{settr(false)}}><h1>hello</h1></ModalPortal>}
+    <button onClick={() => {settr(x => !x)}}>{tr}</button>
+  </>
 }
 
 export default App
