f5b7727e430b0d87ca48a4c026b1ad61318f8dac
Aakri1134 <aakrisri55@gmail.com>
2025-07-29 13:45:32 +0530

    Implement useComponent in Container

--- client/src/DSLComponents/container/Container.tsx
+++ client/src/DSLComponents/container/Container.tsx
@@ -3,6 +3,7 @@ import Convertor from "../../components/Convertor"
 import type { DSLComponent } from "../../utils/DSL/sanetizer"
 import { useSetRecoilState } from "recoil"
 import { activeComponents } from "../../recoil/atoms/component"
+import useComponent from "../../hooks/useComponent"
 
 export interface InputContainer {
   id: DSLComponent["id"]
@@ -30,10 +31,11 @@ export default function Container({
   type,
 }: InputContainer) {
   const setActive = useSetRecoilState(activeComponents)
+  const component = useComponent(id, style, props, parents, children)
 
   const updatedParents = useMemo(() => {
-    return [...parents, id]
-  }, [parents])
+    return [...component.parents, id]
+  }, [component.parents])
 
   useEffect(() => {
     if (typeof id === "string") {
@@ -44,50 +46,50 @@ export default function Container({
   switch (type) {
     case "Article":
       return (
-        <article style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <article style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </article>
       )
     case "Body":
       return (
-        <body style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <body style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </body>
       )
     case "Div":
       return (
-        <div style={{ ...style }} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <div style={{ ...component.style }} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </div>
       )
     case "List":
       return (
-        <ul style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <ul style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </ul>
       )
     case "ListItems":
       return (
-        <li style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <li style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </li>
       )
     case "Main":
       return (
-        <main style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <main style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </main>
       )
     case "Nav":
       return (
-        <nav style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <nav style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </nav>
       )
     case "Section":
       return (
-        <section style={style} className={props.className}>
-          <Convertor components={children} parents={updatedParents} />
+        <section style={component.style} className={component.props.className}>
+          <Convertor components={component.children} parents={updatedParents} />
         </section>
       )
     default:
--- client/src/hooks/useComponent.tsx
+++ client/src/hooks/useComponent.tsx
@@ -1,27 +1,31 @@
 import { useRecoilState } from "recoil"
 import {
+  childrenFamily,
   parentFamily,
   propsFamily,
   styleFamily,
 } from "../recoil/atoms/component"
-import type { DSLComponent } from "../utils/DSL/sanetizer"
+import type { DSL, DSLComponent } from "../utils/DSL/sanetizer"
 import { useEffect } from "react"
 
 export default function useComponent(
   id: string,
   styleInit: DSLComponent["style"],
   propsInit: DSLComponent["props"],
-  parentInit: DSLComponent["id"][]
+  parentInit: DSLComponent["id"][],
+  childrenInit: DSLComponent["children"]
 ) {
   const [style, setStyle] = useRecoilState(styleFamily(id))
   const [props, setProps] = useRecoilState(propsFamily(id))
   const [parents, setParents] = useRecoilState(parentFamily(id))
+  const [children, setChildren] = useRecoilState(childrenFamily(id))
 
   useEffect(() => {
     setStyle(styleInit)
     setProps(propsInit)
     setParents(parentInit)
+    setChildren(childrenInit)
   }, [id, styleInit, propsInit, parentInit, setStyle, setProps, setParents])
 
-  return { style, props, parents, setStyle, setParents, setProps }
+  return { style, props, parents, children, setStyle, setParents, setProps, setChildren }
 }
