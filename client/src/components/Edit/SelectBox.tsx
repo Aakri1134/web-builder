8c4797ff6a8aae4258cd51d7e6a5a21c6726b493
Aakri1134 <aakrisri55@gmail.com>
2025-08-01 01:09:20 +0530

    correct the select box

--- client/src/components/Edit/SelectBox.tsx
+++ client/src/components/Edit/SelectBox.tsx
@@ -1,24 +1,49 @@
 import { useRecoilValue } from "recoil"
-import { selectBox } from "../../recoil/selectors/selectBox"
 import { createPortal } from "react-dom"
+import { currentComponentID, parentFamily } from "../../recoil/atoms/component"
 
 export default function SelectBox() {
-  const boundaries = useRecoilValue(selectBox)
-  console.log(boundaries)
+  const curr = useRecoilValue(currentComponentID)
+  const parent = useRecoilValue(parentFamily(curr ?? "empty"))
+  if (curr === null) return null
 
-  if (boundaries === null) return
-  else
+  let position: "absolute" | "fixed" = "absolute"
+  let i = parent.length - 1
+  if (
+    document.getElementById(curr)?.style.position &&
+    document.getElementById(curr)?.style.position === "fixed"
+  )
+    position = "fixed"
+  while (i >= 0 && position !== "fixed") {
+    if (
+      document.getElementById(parent[i])?.style.position &&
+      document.getElementById(parent[i])?.style.position === "fixed"
+    )
+      position = "fixed"
+    i--
+  }
+  const boundaries = document.getElementById(curr)?.getBoundingClientRect()
+
+  if (boundaries === null || boundaries === undefined) return null
+  else {
     return createPortal(
       <div
         className={` bg-transparent border-4 border-blue-200 rounded-lg z-[1000] absolute pointer-events-none`}
-        style = {{
-            width : boundaries.width,
-            height : boundaries.height,
-            top : window.scrollY + (boundaries.top ?? 0),
-            left : window.scrollX +  (boundaries.left ?? 0)
+        style={{
+          width: boundaries?.width,
+          height: boundaries?.height,
+          top:
+            position === "absolute"
+              ? window.scrollY + (boundaries?.top ?? 0)
+              : boundaries?.top ?? 0,
+          left:
+            position === "absolute"
+              ? window.scrollX + (boundaries?.left ?? 0)
+              : boundaries?.left ?? 0,
+          position: position,
         }}
-        
       />,
       document.body
     )
+  }
 }
