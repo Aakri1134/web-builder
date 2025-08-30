f2282de917aeba1bbe20ad21a2ab211cb2db4748
Aakri1134 <aakrisri55@gmail.com>
2025-08-30 16:26:56 +0530

    Cleaning code

--- client/src/hooks/useComponentClickHandler.tsx
+++ client/src/hooks/useComponentClickHandler.tsx
@@ -1,35 +1,9 @@
-import { useRecoilValue, useSetRecoilState } from "recoil"
+import { useSetRecoilState } from "recoil"
 import type { DSLComponent } from "../utils/DSL/sanetizer"
 import { currentComponentID } from "../recoil/atoms/component"
 import { useRef, useState } from "react"
 
 type ID = DSLComponent["id"]
-
-// ideal version, need to reduce rerenders, but for now ill go with a dumber version
-// export default function useComponentClickHandler(parents: ID[]) {
-//   const [activeComponent, setActiveComponent] =
-//     useRecoilState(currentComponentID)
-
-//   function handleComponentClick() {
-//     console.log(activeComponent)
-
-//     if (activeComponent === null) {
-//       setActiveComponent(parents[parents.length - 1])
-//       return
-//     }
-
-//     let index = parents.indexOf(activeComponent)
-
-//     if (index === -1 || index === 0) {
-//       setActiveComponent(null)
-//       return
-//     }
-
-//     setActiveComponent(parents[index - 1])
-//   }
-
-//   return {handleComponentClick}
-// }
 
 export default function useComponentClickHandler(parents: ID[]) {
   const [index, setIndex] = useState(0)
@@ -46,12 +20,6 @@ export default function useComponentClickHandler(parents: ID[]) {
       setActiveComponent(parents[parents.length - 1 - newIndex])
     }, 250)
   }
-
-  // useEffect(() => {
-  //     if(index != 0){
-  //         alert(index)
-  //     }
-  // }, [index])
 
   const handleParentSelect = () => {
     // called on Double Click
