97f598e7981ac5e7aef3dcf104bee6e7d407679a
Aakri1134 <aakrisri55@gmail.com>
2025-10-05 20:19:59 +0530

    Remove redundant comments

--- client/src/hooks/useStyleInitialValue.tsx
+++ client/src/hooks/useStyleInitialValue.tsx
@@ -17,15 +17,7 @@ export default function useStyleInitialValue<T = any>(
   const logTimeout = useRef<number | undefined>()
 
   function handleLog() {
-    // function isEqual(arr1: any, arr2: any): boolean {
-    //   if (arr1 === arr2) return true
-    //   if (!arr1 || !arr2) return false
-    //   if (arr1.length !== arr2.length) return false
-    //   for (let i = 0; i < arr1.length; i++) {
-    //     if (arr1[i] !== arr2[i]) return false
-    //   }
-    //   return true
-    // }
+    // function isEqual(
 
     function topx(str: string) {
       if (type === "integer" || type === "float") {
